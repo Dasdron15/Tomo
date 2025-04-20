920dfa3dfe7cb86e6389b84f251c66e1a13ccffc
Dasdron15 <ogurechik760@gmail.com>
2025-04-20 22:44:46 +0200

    Add screen draw thread

--- src/main.c
+++ src/main.c
@@ -1,6 +1,21 @@
-#include <curses.h>
 #include "editor.h"
 #include "utils/fileio.h"
+#include <curses.h>
+#include <pthread.h>
+#include <unistd.h>
+
+struct Editor_State* state;
+
+void* draw_loop(void* arg) {
+    const unsigned int fps = 60;
+    const useconds_t delay = 1000000 / fps;
+
+    while (1) {
+        draw_editor(state);
+        usleep(delay);
+    }
+    return NULL;
+}
 
 int main(int argc, char* argv[]) {
     if (argc != 2) {
@@ -8,14 +23,16 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    struct Editor_State* state = malloc(sizeof(struct Editor_State));
+    state = malloc(sizeof(struct Editor_State));
     state->filename = argv[1];
 
     load_file(argv[1], state);
     init_editor(state);
     
+    pthread_t draw_thread;
+    pthread_create(&draw_thread, NULL, draw_loop, NULL);
+
     while (1) {
-        draw_editor(state);
         int key = getch();
         move_cursor(key, state);
         handle_key(key, state);
