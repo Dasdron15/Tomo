ed8944e0e326bb12a0d30639035d2a9c3e3c14a7
Dasdron15 <ogurechik760@gmail.com>
2025-07-05 16:16:24 +0300

    Added clamping for y axis

--- src/cursor.c
+++ src/cursor.c
@@ -25,12 +25,12 @@ void move_up(bool is_selecting) {
 }
 
 void move_down(bool is_selecting) {
-    if (cursor.y + cursor.y_offset < editor.total_lines) {
+    if (cursor.y + cursor.y_offset < editor.total_lines - 1) {
         cursor.y++;
         cursor.x = cursor.max_x;
     } else {
         size_t line_len = strlen(editor.lines[cursor.y + cursor.y_offset]);
-        cursor.x = line_len;
+        cursor.x = line_len + editor.margin;
         cursor.max_x = cursor.x;
     }
 
@@ -82,8 +82,20 @@ void move_left(bool is_selecting) {
 
 void clamp_cursor(void) {
     size_t line_len = strlen(editor.lines[cursor.y + cursor.y_offset]);
-    
-    if (cursor.x + cursor.x_offset > line_len) {
+    size_t screen_width = getmaxx(stdscr);
+    size_t screen_height = getmaxy(stdscr);
+
+    if (cursor.x + cursor.x_offset > line_len + editor.margin) {
         cursor.x = line_len + editor.margin;
+    }
+
+    if (cursor.y < 3 && cursor.y_offset > 0) {
+        cursor.y_offset -= 3 - cursor.y;
+        cursor.y = 3;
+    }
+
+    if (cursor.y > screen_height - 5 && editor.total_lines > screen_height + cursor.y_offset - 1) {
+        cursor.y_offset += cursor.y - (screen_height - 5);
+        cursor.y = screen_height - 5;
     }
 }
