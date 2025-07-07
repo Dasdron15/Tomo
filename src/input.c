6763bca1841c31d51bf0048f8a55324078970e33
Dasdron15 <ogurechik760@gmail.com>
2025-07-07 12:05:22 +0300

    Moved handle_input function into input.c file

---
+++ src/input.c
@@ -0,0 +1,233 @@
+#include "input.h"
+
+#include <curses.h>
+#include <stdlib.h>
+
+#include "cursor.h"
+#include "select.h"
+#include "editor.h"
+#include "edit.h"
+#include "fileio.h"
+
+void handle_key(int key) {
+    if (key == KEY_UP) {
+        move_up(false);
+        return;
+    }
+
+    if (key == KEY_DOWN) {
+        move_down(false);
+        return;
+    }
+
+    if (key == KEY_RIGHT) {
+        move_right(false);
+        return;
+    }
+
+    if (key == KEY_LEFT) {
+        move_left(false);
+        return;
+    }
+
+    if (key == 402) { // Shift + RIGHT_ARROW (Right arrow selection)
+        start_selection(cursor.y + cursor.y_offset,
+                        cursor.x - editor.margin + cursor.x_offset);
+        move_right(true);
+        update_selection(cursor.y + cursor.y_offset,
+                         cursor.x - editor.margin + cursor.x_offset);
+        return;
+    }
+
+    if (key == 393) { // Shift + LEFT_ARROW (Left arrow selection)
+        start_selection(cursor.y + cursor.y_offset,
+                        cursor.x - editor.margin + cursor.x_offset);
+        move_left(true);
+        update_selection(cursor.y + cursor.y_offset,
+                         cursor.x - editor.margin + cursor.x_offset);
+        return;
+    }
+
+    if (key == 337) { // Shift + UP_ARROW (Up arrow selection)
+        start_selection(cursor.y + cursor.y_offset,
+                        cursor.x - editor.margin + cursor.x_offset);
+        move_up(true);
+        update_selection(cursor.y + cursor.y_offset,
+                         cursor.x - editor.margin + cursor.x_offset);
+        return;
+    }
+
+    if (key == 336) { // Shift + DOWN_ARROW (Down arrow selection)
+        start_selection(cursor.y + cursor.y_offset,
+                        cursor.x - editor.margin + cursor.x_offset);
+        move_down(true);
+        update_selection(cursor.y + cursor.y_offset,
+                         cursor.x - editor.margin + cursor.x_offset);
+        return;
+    }
+
+    if (key == 27) { // Esc
+        cancel_selection();
+        return;
+    }
+
+    if (key == 17) { // CTRL + Q (Quit the editor)
+        if (!editor.is_saved) {
+            ask_for_save();
+            return;
+        }
+        printf("\033[?1006l");
+
+        endwin();
+        exit(0);
+        return;
+    }
+
+    if ((key >= 32 && key <= 126)) {
+        insert_char((char)key);
+        editor.is_saved = false;
+        return;
+    }
+
+    if (key == '\t') {
+        add_tab();
+        cursor.max_x = cursor.x;
+        editor.is_saved = false;
+        return;
+    }
+
+    if (key == KEY_BACKSPACE || key == 127) {
+        int x_pos = cursor.x + cursor.x_offset - editor.margin;
+        int y_pos = cursor.y + cursor.y_offset;
+
+        Point start_select;
+        Point end_select;
+
+        if (!is_selecting()) {
+            start_select.x = x_pos - 1;
+            start_select.y = y_pos;
+
+            end_select.x = x_pos - 1;
+            end_select.y = y_pos;
+        } else if (is_selecting() &&
+                   ((get_start().x > x_pos && get_start().y == y_pos) ||
+                    (get_start().y > y_pos))) {
+            start_select.x = x_pos;
+            start_select.y = y_pos;
+
+            end_select.x = get_start().x;
+            end_select.y = get_start().y;
+        } else if (is_selecting() &&
+                   ((get_start().x < x_pos && get_start().y == y_pos) ||
+                    (get_start().y < y_pos))) {
+            start_select.x = get_start().x;
+            start_select.y = get_start().y;
+
+            end_select.x = x_pos;
+            end_select.y = y_pos;
+        } else {
+            return;
+        }
+
+        deletion(start_select, end_select);
+        editor.is_saved = false;
+        return;
+    }
+
+    if (key == KEY_ENTER || key == 10) {
+        new_line();
+        editor.is_saved = false;
+        return;
+    }
+
+    if (key == 19) { // CTRL + S (Save file)
+        save_file();
+        return;
+    }
+
+    if (key == 7) { // CTRL + G (Goto line)
+        int target = goto_line();
+        if (target != -1) {
+            cursor.y = target - cursor.y_offset;
+            clamp_cursor();
+        }
+        return;
+    }
+
+    if (key == 534 &&
+        cursor.y + cursor.y_offset <
+            editor.total_lines +
+                1) { // CTRL + DOWN_ARROW (Jump to the end of the file)
+        if (editor.total_lines > getmaxy(stdscr) - 1) {
+            cursor.y_offset = editor.total_lines - getmaxy(stdscr) + 1;
+        }
+        cursor.y = editor.total_lines - cursor.y_offset - 1;
+        clamp_cursor();
+    }
+
+    if (key == 575) { // CTRL + UP_ARROW (Jump to the beggining of the file)
+        cursor.y_offset = 0;
+        cursor.y = 0;
+        clamp_cursor();
+    }
+
+    if (key == 3 && is_selecting()) { // CTRL + C (Copy)
+        int x_pos = cursor.x + cursor.x_offset - editor.margin;
+        int y_pos = cursor.y + cursor.y_offset;
+
+        Point start_select;
+        Point end_select;
+        
+        if (is_selecting() &&
+                   ((get_start().x > x_pos && get_start().y == y_pos) ||
+                    (get_start().y > y_pos))) {
+            start_select.x = x_pos;
+            start_select.y = y_pos;
+
+            end_select.x = get_start().x;
+            end_select.y = get_start().y;
+        } else if (is_selecting() &&
+                   ((get_start().x < x_pos && get_start().y == y_pos) ||
+                    (get_start().y < y_pos))) {
+            start_select.x = get_start().x;
+            start_select.y = get_start().y;
+
+            end_select.x = x_pos;
+            end_select.y = y_pos;
+        }
+
+        copy_text(start_select, end_select);
+    }
+
+    if (key == 24 && is_selecting()) { // CTRL + X (Cut)
+        int x_pos = cursor.x + cursor.x_offset - editor.margin;
+        int y_pos = cursor.y + cursor.y_offset;
+
+        Point start_select;
+        Point end_select;
+        
+        if (is_selecting() &&
+                   ((get_start().x > x_pos && get_start().y == y_pos) ||
+                    (get_start().y > y_pos))) {
+            start_select.x = x_pos;
+            start_select.y = y_pos;
+
+            end_select.x = get_start().x;
+            end_select.y = get_start().y;
+        } else if (is_selecting() &&
+                   ((get_start().x < x_pos && get_start().y == y_pos) ||
+                    (get_start().y < y_pos))) {
+            start_select.x = get_start().x;
+            start_select.y = get_start().y;
+
+            end_select.x = x_pos;
+            end_select.y = y_pos;
+        }
+
+        copy_text(start_select, end_select);
+        deletion(start_select, end_select);
+
+        editor.is_saved = false;
+    }
+}
+
