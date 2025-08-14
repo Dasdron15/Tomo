5ef68c9401c8f69adc40e138a384273e3df339cc
Dasdron15 <ogurechik760@gmail.com>
2025-08-14 17:05:55 +0300

    command palette layout done

--- src/command_palette.c
+++ src/command_palette.c
@@ -7,25 +7,33 @@ void draw_command_palette(void) {
     int max_x, max_y;
     getmaxyx(stdscr, max_y, max_x);
 
-    int height = 6;
+    const char *options[] = {"Change theme", "Go to line", "Save file", "Exit"};
+    int n_options = sizeof(options) / sizeof(options[0]);
+    int highlight = 0;
+
+    int pos = 0;
+    char buf[64] = {0};
+
+    int height = n_options + 3; // 3 - Borders + input line
     int width = max_x - 20;
     int startx = (max_x - width) / 2;
-    int starty = (max_y - height) / 2;
+    int starty = max_y / 10;
 
     WINDOW *palette = newwin(height, width, starty, startx);
     keypad(palette, TRUE);
     wbkgd(palette, COLOR_PAIR(PAIR_BACKGROUND));
 
-    const char *options[] = {"Change theme", "Save file", "Exit"};
-    int n_options = sizeof(options) / sizeof(options[0]);
-    int highlight = 0;
-
     while (1) {
         getmaxyx(stdscr, max_y, max_x);
         width = max_x - 20;
-        height = n_options + 2;
+        height = n_options + 3;
+
+        if (width < 1 || height < 1) {
+            continue;
+        }
+
         startx = (max_x - width) / 2;
-        starty = (max_y - height) / 2;
+        starty = max_y / 10;
         wresize(palette, height, width);
         mvwin(palette, starty, startx);
 
@@ -35,13 +43,16 @@ void draw_command_palette(void) {
         for (int i = 0; i < n_options; i++) {
             if (i == highlight) {
                 wattron(palette, COLOR_PAIR(PAIR_STATUS_BAR));
+                mvwhline(palette, i + 2, 1, ' ', width - 2);
             }
-            mvwprintw(palette, i + 1, 2, "%s", options[i]);
-            wattroff(palette, COLOR_PAIR(PAIR_STATUS_BAR));
-        }
+            mvwprintw(palette, i + 2, 2, "%s", options[i]);
+            if (i == highlight) {
+                wattroff(palette, COLOR_PAIR(PAIR_STATUS_BAR));
+            }
 
-        wrefresh(palette);
-        wrefresh(stdscr);
+            mvwprintw(palette, 1, 1, "> ");
+            mvwprintw(palette, 1, 3, "%s", buf);
+        }
 
         int ch = wgetch(palette);
         if (ch == KEY_UP) {
@@ -49,11 +60,18 @@ void draw_command_palette(void) {
         } else if (ch == KEY_DOWN) {
             highlight = (highlight + 1) % n_options;
         } else if (ch == '\n') {
-            delwin(palette);
             break;
         } else if (ch == 27) {
-            delwin(palette);
             break;
+        } else if (ch > 31 && ch < 127 && pos < (int)sizeof(buf) - 1) {
+            buf[pos++] = ch;
+            buf[pos] = '\0';
+        } else if ((ch == 127 || ch == KEY_BACKSPACE) && pos > 0) {
+            buf[--pos] = '\0';
         }
+
+        refresh();
     }
+
+    delwin(palette);
 }
