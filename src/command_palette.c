9b1aecfda8c0f35ff55f4039046ae261e112662a
Dasdron15 <ogurechik760@gmail.com>
2025-08-15 00:13:38 +0300

    Refactored command palette code

--- src/command_palette.c
+++ src/command_palette.c
@@ -9,18 +9,12 @@
 #include "editor.h"
 #include "themes.h"
 
-int draw_command_palette(void) {
+static int draw_palette(char **items, int n_items, char *buf, size_t buf_size) {
     int max_x, max_y;
     getmaxyx(stdscr, max_y, max_x);
 
-    const char *options[] = {"Change theme", "Go to line", "Save file", "Exit"};
-    int n_options = sizeof(options) / sizeof(options[0]);
-    int highlight = 0;
-
-    int pos = 0;
-    char buf[64] = {0};
-
-    int height = n_options + 3; // 3 - Borders + input line
+    int highlight = 0, pos = 0;
+    int height = n_items + 3;
     int width = max_x - 20;
     int startx = (max_x - width) / 2;
     int starty = max_y / 10;
@@ -32,12 +26,7 @@ int draw_command_palette(void) {
     while (1) {
         getmaxyx(stdscr, max_y, max_x);
         width = max_x - 20;
-        height = n_options + 3;
-
-        if (width < 1 || height < 1) {
-            continue;
-        }
-
+        height = n_items + 3;
         startx = (max_x - width) / 2;
         starty = max_y / 10;
         wresize(palette, height, width);
@@ -46,48 +35,43 @@ int draw_command_palette(void) {
         werase(palette);
         box(palette, 0, 0);
 
-        for (int i = 0; i < n_options; i++) {
+        for (int i = 0; i < n_items; i++) {
             if (i == highlight) {
                 wattron(palette, COLOR_PAIR(PAIR_STATUS_BAR));
                 mvwhline(palette, i + 2, 1, ' ', width - 2);
             }
-            mvwprintw(palette, i + 2, 2, "%s", options[i]);
-            if (i == highlight) {
-                wattroff(palette, COLOR_PAIR(PAIR_STATUS_BAR));
+
+            char *theme_name = strrchr(items[i], '/');
+            if (theme_name) {
+                theme_name++;
+            } else {
+                theme_name = items[i];
             }
 
-            mvwprintw(palette, 1, 1, "> ");
-            mvwprintw(palette, 1, 3, "%s", buf);
+            mvwprintw(palette, i + 2, 2, "%s", theme_name);
+            if (i == highlight) wattroff(palette, COLOR_PAIR(PAIR_STATUS_BAR));
         }
 
+        mvwprintw(palette, 1, 1, "> ");
+        mvwprintw(palette, 1, 3, "%s", buf);
+
         int ch = wgetch(palette);
-        if (ch == KEY_UP) {
-            highlight = (highlight - 1 + n_options) % n_options;
-        } else if (ch == KEY_DOWN) {
-            highlight = (highlight + 1) % n_options;
-        } else if (ch == '\n') {
-            delwin(palette);
-            touchwin(stdscr);
-            wrefresh(stdscr);
-            refresh();
-            draw_editor();
-            return highlight;
-        } else if (ch == 27) {
-            delwin(palette);
-            touchwin(stdscr);
-            wrefresh(stdscr);
-            draw_editor();
-            return -1;
-        } else if (ch > 31 && ch < 127 && pos < (int)sizeof(buf) - 1) {
-            buf[pos++] = ch;
-            buf[pos] = '\0';
-        } else if ((ch == 127 || ch == KEY_BACKSPACE) && pos > 0) {
-            buf[--pos] = '\0';
-        }
+        if (ch == KEY_UP) highlight = (highlight - 1 + n_items) % n_items;
+        else if (ch == KEY_DOWN) highlight = (highlight + 1) % n_items;
+        else if (ch == '\n') { delwin(palette); return highlight; }
+        else if (ch == 27)    { delwin(palette); return -1; }
+        else if (ch > 31 && ch < 127 && pos < (int)buf_size - 1) buf[pos++] = ch, buf[pos] = '\0';
+        else if ((ch == 127 || ch == KEY_BACKSPACE) && pos > 0) buf[--pos] = '\0';
 
         draw_editor();
         refresh();
     }
+}
+
+int draw_command_palette(void) {
+    char *options[] = {"Change theme", "Go to line", "Save file", "Exit"};
+    char buf[64] = {0};
+    return draw_palette(options, sizeof(options) / sizeof(options[0]), buf, sizeof(buf));
 }
 
 char *draw_theme_palette(void) {
@@ -115,96 +99,12 @@ char *draw_theme_palette(void) {
         }
         closedir(d);
     }
-
+    
     if (n_themes == 0) {
         editor.bottom_text = "No themes found in ~/.config/tomo/themes";
         return NULL;
     }
-
-    int max_x, max_y;
-    getmaxyx(stdscr, max_y, max_x);
-
-    int highlight = 0;
-
-    int pos = 0;
     char buf[64] = {0};
-
-    int height = n_themes + 3; // 3 - Borders + input line
-    int width = max_x - 20;
-    int startx = (max_x - width) / 2;
-    int starty = max_y / 10;
-
-    WINDOW *palette = newwin(height, width, starty, startx);
-    keypad(palette, TRUE);
-    wbkgd(palette, COLOR_PAIR(PAIR_BACKGROUND));
-
-    while (1) {
-        getmaxyx(stdscr, max_y, max_x);
-        width = max_x - 20;
-        height = n_themes + 3;
-
-        if (width < 1 || height < 1) {
-            continue;
-        }
-
-        startx = (max_x - width) / 2;
-        starty = max_y / 10;
-        wresize(palette, height, width);
-        mvwin(palette, starty, startx);
-
-        werase(palette);
-        box(palette, 0, 0);
-
-        for (int i = 0; i < n_themes; i++) {
-            if (i == highlight) {
-                wattron(palette, COLOR_PAIR(PAIR_STATUS_BAR));
-                mvwhline(palette, i + 2, 1, ' ', width - 2);
-            }
-
-            char *theme_name = strrchr(themes[i], '/');
-            if (theme_name) {
-                theme_name++;
-            } else {
-                theme_name = themes[i];
-            }
-
-            mvwprintw(palette, i + 2, 2, "%s", theme_name);
-            if (i == highlight) {
-                wattroff(palette, COLOR_PAIR(PAIR_STATUS_BAR));
-            }
-
-            mvwprintw(palette, 1, 1, "> ");
-            mvwprintw(palette, 1, 3, "%s", buf);
-        }
-
-        int ch = wgetch(palette);
-        if (ch == KEY_UP) {
-            highlight = (highlight - 1 + n_themes) % n_themes;
-        } else if (ch == KEY_DOWN) {
-            highlight = (highlight + 1) % n_themes;
-        } else if (ch == '\n') {
-            delwin(palette);
-            touchwin(stdscr);
-            wrefresh(stdscr);
-            refresh();
-            draw_editor();
-            return themes[highlight];
-
-        } else if (ch == 27) {
-            delwin(palette);
-            touchwin(stdscr);
-            wrefresh(stdscr);
-            draw_editor();
-            return NULL;
-
-        } else if (ch > 31 && ch < 127 && pos < (int)sizeof(buf) - 1) {
-            buf[pos++] = ch;
-            buf[pos] = '\0';
-        } else if ((ch == 127 || ch == KEY_BACKSPACE) && pos > 0) {
-            buf[--pos] = '\0';
-        }
-
-        draw_editor();
-        refresh();
-    }
+    int idx = draw_palette(themes, n_themes, buf, sizeof(buf));
+    return idx >= 0 ? themes[idx] : NULL;
 }
