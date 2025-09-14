1f5da34cf98cc46b536e5a6e15cb06c6d0ad9580
Dasdron15 <ogurechik760@gmail.com>
2025-09-14 23:00:38 +0400

    Add file listing in a new window

--- src/file_tree.c
+++ src/file_tree.c
@@ -1,10 +1,29 @@
 #include "file_tree.h"
 
 #include <curses.h>
+#include <stdlib.h>
 #include <stdio.h>
 
 void draw_tree(char **files, int el_num) {
-    for (int i = 0; i < el_num; i++) {
-        printf("%s\n", files[i]);
+    initscr();
+
+    int max_x, max_y;
+    getmaxyx(stdscr, max_y, max_x);
+
+    WINDOW *tree_win = newwin(max_y, max_x, 0, 0);
+
+    noecho();
+
+    int ch;
+    while ((ch = getch()) != 27) {
+        wclear(tree_win);
+        for (int i = 0; i < el_num; i++) {
+            mvwprintw(tree_win, i, 0, "%s", files[i]);
+        }
+        wrefresh(tree_win);
     }
+
+    delwin(tree_win);
+    endwin();
+    exit(0);
 }
